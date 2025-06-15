e41a83ac797f0445b400714b141e0e255752a0eb
AnnaChason <annachason.wiggins@my.wheaton.edu>
2025-06-15 13:29:25 -0500

    buttons!!

--- jsFiles/card.js
+++ jsFiles/card.js
@@ -1,1 +1,30 @@
 /*javascript file for managing flashcard on the home screen */
+var flipped = false;
+function flipCard(){
+    const card = document.getElementById("idxCard");
+    if(flipped){
+        card.style.backgroundImage = `url(imgs/cardFrame.png)`;
+    }else{
+        card.style.backgroundImage = `url(imgs/cardFrameBack.png)`;
+    }
+    flipped = !flipped;
+}
+
+function forward(){
+    const card = document.getElementById("idxCard");
+    if(flipped){
+        card.style.backgroundImage = `url(imgs/cardFrame.png)`;
+    }else{
+        card.style.backgroundImage = `url(imgs/cardFrameBack.png)`;
+    }
+    flipped = !flipped;
+}
+function back(){
+    const card = document.getElementById("idxCard");
+    if(flipped){
+        card.style.backgroundImage = `url(imgs/cardFrame.png)`;
+    }else{
+        card.style.backgroundImage = `url(imgs/cardFrameBack.png)`;
+    }
+    flipped = !flipped;
+}
