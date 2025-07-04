dd960c5b76c0a60ff96a5c23b64c011dfacecf8f
AnnaChason <annachason.wiggins@my.wheaton.edu>
2025-07-03 21:30:43 -0500

    connected cards to database, and it is now possible to flip through them. (can go out of bounds though)

--- jsFiles/card.js
+++ jsFiles/card.js
@@ -1,45 +1,70 @@
 /*javascript file for managing flashcard on the home screen */
 import {supabase} from '../jsFiles/supabaseClient.js';
 
+const titleTxt = document.getElementById("deckTitleText");
+const cardTxt = document.getElementById("cardText");
 
 /* 
 card navigation management
 */
+    var currentCard = 0;
+    var cards = null;
+
     document.getElementById('flipBtn').addEventListener('click', flipCard);
     document.getElementById('forwardBtn').addEventListener('click', forward);
     document.getElementById('backBtn').addEventListener('click', back);
+    const card = document.getElementById("idxCard");
 
     var flipped = false;
     function flipCard(){
-        console.log("flip");
-        const card = document.getElementById("idxCard");
-        if(flipped){
+        console.log(cards);
+        if(flipped){//going to front
             card.style.backgroundImage = `url(imgs/cardFrame.png)`;
-        }else{
+            if(cards != null)
+                cardTxt.innerText=cards[currentCard].term;
+        }else{//going to back
             card.style.backgroundImage = `url(imgs/cardFrameBack.png)`;
+            if(cards != null && currentCard < cards.length)
+                cardTxt.innerText=cards[currentCard].definition;
         }
         flipped = !flipped;
     }
 
     function forward(){
-        console.log("forward");
-        const card = document.getElementById("idxCard");
+        console.log(cards);
+        currentCard ++;
         if(flipped){
             card.style.backgroundImage = `url(imgs/cardFrame.png)`;
-        }else{
-            card.style.backgroundImage = `url(imgs/cardFrameBack.png)`;
+            flipped = false;
         }
-        flipped = !flipped;
+        if(cards != null)
+            cardTxt.innerText=cards[currentCard].term;
     }
     function back(){
-        console.log("back");
-        const card = document.getElementById("idxCard");
+        console.log(cards);
+        currentCard --;
         if(flipped){
             card.style.backgroundImage = `url(imgs/cardFrame.png)`;
-        }else{
-            card.style.backgroundImage = `url(imgs/cardFrameBack.png)`;
+            flipped = false;
         }
-        flipped = !flipped;
+        if(cards != null && currentCard < cards.length)
+            cardTxt.innerText=cards[currentCard].term;
+    }
+
+/*
+loads card data
+*/
+    async function loadDeck(id){
+        const deck = await supabase
+        .from('deck')
+        .select('name,cards')
+        .eq('id', id)
+        .order('created_at');
+
+        titleTxt.innerText=deck.data[0].name;
+        cards = deck.data[0].cards;
+        console.log(cards);
+        cardTxt.innerText = cards[0].term;
     }
 
 /*
@@ -58,18 +83,22 @@ manages the deck selection popup
     //load deck option titles
     async function loadTitles(){
         const titles = await supabase
-    .from('deck')
-    .select('name')
-    .order('created_at')
+        .from('deck')
+        .select('name,id')
+        .order('created_at')
 
-    for(let i = 0; i < titles.data.length; i++){
-        var row = document.createElement("div");
-        var text = document.createElement("p");
-        text.innerText = titles.data[i].name;
-        row.appendChild(text);
-        row.addEventListener("click", ()=>{ popupdiv.style.display="none"; backShadow.style.display="none";})
-        popupdiv.appendChild(row);
-    }
+        for(let i = 0; i < titles.data.length; i++){
+            var row = document.createElement("div");
+            var text = document.createElement("p");
+            text.innerText = titles.data[i].name;
+            row.appendChild(text);
+            row.addEventListener("click", ()=>{ 
+                popupdiv.style.display="none"; 
+                backShadow.style.display="none"; 
+                loadDeck(titles.data[i].id);
+            });
+            popupdiv.appendChild(row);
+        }
     }
 
 
