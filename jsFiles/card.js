6a64f50ec03dd835c0f1130780a2ef8dd8ea1d17
AnnaChason <annachason.wiggins@my.wheaton.edu>
2025-07-16 17:13:22 -0500

    began working on implementing different ways to study

--- jsFiles/card.js
+++ jsFiles/card.js
@@ -1,49 +1,44 @@
-/*javascript file for managing flashcard on the home screen */
+/*
+javascript file for managing flashcard on the home screen 
+*/
 import {supabase} from '../jsFiles/supabaseClient.js';
 
-const titleTxt = document.getElementById("deckTitleText");
-const cardTxt = document.getElementById("cardText");
-const rightBtn = document.getElementById("right");
-const wrongBtn = document.getElementById("wrong");
-
-/* 
-card navigation management
+/*
+connect to html
 */
-    var currentCard = 0;
-    var cards = null;
+    const titleTxt = document.getElementById("deckTitleText");
+    const cardTxt = document.getElementById("cardText");
+    const rightBtn = document.getElementById("right");
+    const wrongBtn = document.getElementById("wrong");
     const cardIdxText = document.getElementById("cardIdx");
     const deckLength = document.getElementById("deckLength");
-
-    document.getElementById('flipBtn').addEventListener('click', flipCard);
-    document.getElementById('forwardBtn').addEventListener('click', forward);
-    document.getElementById('backBtn').addEventListener('click', back);
     const card = document.getElementById("idxCard");
 
+//set up
+var currentCard = 0;
+var cards = null;
+loadRecentDeck();
+
+/*
+manages different ways to study the flashcards
+*/
+// shared
     var flipped = false;
     function flipCard(){
-        console.log(cards);
         if(flipped){//going to front
-            rightBtn.style.display="none";
-            wrongBtn.style.display="none";
-
             card.style.backgroundImage = `url(imgs/cardFrame.png)`;
             if(cards != null)
                 cardTxt.innerText=cards[currentCard].term;
         }else{//going to back
-            rightBtn.style.display="block";
-            wrongBtn.style.display="block";
-
             card.style.backgroundImage = `url(imgs/cardFrameBack.png)`;
             if(cards != null && currentCard < cards.length)
                 cardTxt.innerText=cards[currentCard].definition;
         }
         flipped = !flipped;
+        return !flipped;
     }
 
     function forward(){
-        rightBtn.style.display="none";
-        wrongBtn.style.display="none";
-
         currentCard ++;
         if(currentCard < 0){
             currentCard = cards.length-1;
@@ -61,9 +56,6 @@ card navigation management
             cardTxt.innerText=cards[currentCard].term;
     }
     function back(){
-        rightBtn.style.display="none";
-        wrongBtn.style.display="none";
-
         currentCard --;
         if(currentCard < 0){
             currentCard = cards.length-1;
@@ -81,6 +73,82 @@ card navigation management
             cardTxt.innerText=cards[currentCard].term;
     }
 
+    function randomizeOrder(){  
+        var randomized = [];
+        while(cards.length > 0){
+            const rmIdx = Math.floor(Math.random()*cards.length-1);
+            randomized.push(cards.splice(rmIdx,1)[0]);
+        }
+        cards = [...randomized];
+        console.log(cards);
+    }
+/*
+    strategies: 
+*/
+    //in order
+    class InOrder{
+        onSelect(){
+            //to do: return cards to og order
+        }
+        flip(){
+          flipCard();  
+        }
+        next(){
+            forward();
+        }
+        prev(){
+            back();
+        }
+    }
+    //random order
+    class RandOrder{
+        onSelect(){
+            randomizeOrder();
+        }
+        flip(){
+          flipCard();  
+        }
+        next(){
+            forward();
+        }
+        prev(){
+             randomizeOrder();
+            back();
+        }
+    }
+    //dynamic study
+    class Dynamic{
+        onSelect(){
+            randomizeOrder();
+        }
+        flip(){
+          const front = flipCard();  
+          if(front){
+            rightBtn.style.display="none";
+            wrongBtn.style.display="none";
+          }
+          else{
+            rightBtn.style.display="block";
+            wrongBtn.style.display="block";
+          }
+        }
+        next(){//correct
+            rightBtn.style.display="none";
+            wrongBtn.style.display="none";
+            forward();
+        }
+        prev(){//incorrect
+            rightBtn.style.display="none";
+            wrongBtn.style.display="none";
+            back();
+        }
+    }
+
+    var strat = new RandOrder();
+
+    document.getElementById('flipBtn').addEventListener('click', strat.flip);
+    document.getElementById('forwardBtn').addEventListener('click', strat.next);
+    document.getElementById('backBtn').addEventListener('click', strat.prev);
 /*
 loads card data
 */
@@ -90,6 +158,24 @@ loads card data
         .select('name,cards')
         .eq('id', id)
         .order('created_at');
+
+        titleTxt.innerText=deck.data[0].name;
+        cards = deck.data[0].cards;
+        cardTxt.innerText = cards[0].term;
+        
+        currentCard = 0;
+        cardIdxText.innerText = 1;
+        deckLength.innerText = cards.length;
+    }
+/*
+load most recently created deck
+*/
+    async function loadRecentDeck(){
+        const deck = await supabase
+        .from('deck')
+        .select('name,cards')
+        .order('created_at', {ascending: false})
+        .limit(1);
 
         titleTxt.innerText=deck.data[0].name;
         cards = deck.data[0].cards;
